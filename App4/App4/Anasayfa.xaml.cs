5bf516c0c51f40ec12d3f4ecb2b2d15ed1f8755c
Onur Keçe <onurkecee@gmail.com>
2021-03-21 22:42:36 +0300

    Xamarin Desi Hesaplama

---
+++ App4/App4/Anasayfa.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Anasayfa : ContentPage
+    {
+        public Anasayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnHesapla(object o, EventArgs args)
+        {
+            if (en.Text == null || boy.Text == null || yukseklik.Text == null)
+            {
+                DisplayAlert("HATA", "Lütfen Boş Alanları Doldurunuz.", "Tamam");
+            }
+            else
+            {
+                float desisonuc = (float.Parse(en.Text) * float.Parse(boy.Text) * float.Parse(yukseklik.Text)) / 3000;
+                sonuc.Text = "Sonuc: " + desisonuc;
+            }
+            
+        }
+    }
+}
---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void BtnGiris(object o, EventArgs args)
+        {
+            if (username.Text=="onur" && password.Text=="123")
+            {
+                await Navigation.PushModalAsync(new Anasayfa());
+            }
+            else
+            {
+                await DisplayAlert("Uyarı", "Hatalı Giriş", "Tamam");
+            }
+        }
+    }
+}
